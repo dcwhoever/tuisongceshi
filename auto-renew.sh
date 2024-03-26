a69fa16a20e1e3c29afcf0623ddd32f6134c0539
dcwhoever <154269055+dcwhoever@users.noreply.github.com>
2024-03-26 15:41:05 +0800

    Update file auto-renew.sh

---
+++ auto-renew.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while true; do
+  sshpass -p 'nb#xFwTPUf06Q5S*P)Um' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt Richards993@s1.serv00.com "exit"       #自己6
+  sleep 5
+  sshpass -p 'Cq&#XeOMgX)OnvQrCP7n' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt smileh81317@s1.serv00.com "exit"      #保1服务器
+  sleep 5
+  sshpass -p '5Rpox%$(5yUq!blU4NKl' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt glassrooomha@s1.serv00.com "exit"      #保2服务器
+  sleep 5
+  sshpass -p '*Z!hIK60DlRbop)FUiCb' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt oveto00o0o0o0o@s1.serv00.com "exit"     #保7服务器
+  sleep 5
+  sshpass -p '4Ds&VAdD9(jEB4cKfP9y' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt vzzmeywwv@s1.serv00.com "exit"      #保8服务器
+  sleep 5
+  sshpass -p 'HpfYWBDkHEQLA0eZVu!s' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt dm132h7@s2.serv00.com "exit"      #保9服务器
+  sleep 5
+  sshpass -p 'UeMoWXvt0o!OTVdlNMya' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt vplifebe@s2.serv00.com "exit"     #保10服务器
+  sleep 5
+  sshpass -p '1qP1FVFtDhyht(u%Oq3#' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt Fsdx332@s2.serv00.com "exit"      #保11服务器
+  sleep 5
+  sshpass -p '(LmtPd%d(3Hdm5UME^7v' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt oo00OO00oo00O0oo@s2.serv00.com "exit"      #保12服务器
+  sleep 5
+  sleep 55920  #3天为259200秒   此处是7.2小时
+done
